3fea57de540156897af7e66b5d097f1a57bc08ca
Ironboy <ironboy@users.noreply.github.com>
2026-01-14 15:46:23 +0100

    refactored old-school click handler to a modern one

--- js/create-header.js
+++ js/create-header.js
@@ -4,10 +4,14 @@ export default async function createHeader(header) {
     <div class="logo">Our Company</div>
     <input type="checkbox" id="menu-toggle">
     <label for="menu-toggle">☰</label>
-    <nav onclick="document.querySelector('#menu-toggle').checked = false">
+    <nav>
       ${menuItems.map(({ label, slug }) => /*html*/`
          <a href="#${slug}">${label}</a>
       `).join('')}
     </nav>
   `;
+
+  // add a on click handler that closes the hamburger menu
+  header.querySelector('nav').addEventListener('click',
+    () => document.querySelector('#menu-toggle').checked = false);
 }

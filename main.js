4fac10372d87b5de9b744acfccfd17900cb6b623
Ironboy <ironboy@users.noreply.github.com>
2026-01-14 10:59:54 +0100

    product list loop creating html

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
+let products = [
+  { name: 'Ball', price: 35, description: 'A nice rubber ball.' },
+  { name: 'Rubber duck', price: 105, description: 'For extensive rubber ducking.' },
+  { name: 'Coffee', price: 35, description: 'To prevent premature sleep and make you happy.' }
+];
+
 // Grab the h1 element 
 let h1 = document.querySelector('h1');
 
 // Change the text of the h1 element
-h1.textContent = 'Hello from JavaScript!';
+h1.textContent = 'Our shop';
 
-// Create an article element 
-let article = document.createElement('article');
+// Create an main element 
+let main = document.createElement('main');
 
-// Change/add text to the p element
-article.innerHTML = `
-<p>This is a p element from JS. I can also add lists from JS:</p>
-<ul>
-  <li>I am a bullet point and I'm okay</li>
-  <li>I am another one</li>
-</ul>
+// Loop through products and build some html
+let html = '';
+for (let product of products) {
+  html += `
+    <article>
+      <h3>${product.name}</h3>
+      <p>${product.description}</p>
+      <p><b>Price:</b> ${product.price}</p>
+    </article>
+  `;
+}
+
+// Change/add html to the main element
+// `` = template literal multiline string
+//      in which you also interpolate expressions/variables
+// To get some syntax highlightning of HTML in a template literal
+// you can install the VSC extension leet-html 
+main.innerHTML = `
+<p>Our amazing products:</p>
+${html}
 `;
 
 // Add the p element somewhere in the DOM
-document.querySelector('body').append(article);
+document.querySelector('body').append(main);
